1ea5a4d1b4712d3597e85ecd2c49e50f75c8de18
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-10 12:24:07 +0200

    Add project files.

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2205240#content
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Access Modifiers:
+             * Public
+             * Private
+             * Protected
+             * Internal
+             * Protected Internal
+             */
+
+            // Access Modifier is a way to control access to a class and/or its members
+            // it's used to create safety in programs
+
+            // Concepts of object-oriented programming
+            // * encapsulation - or Information Hiding
+            // * Inheritance
+            // * Polymorphism
+
+            // Encapsulation in practice
+            // * Define fields as private
+            // * Provide getter/setter methods as public
+        }
+    }
+}
